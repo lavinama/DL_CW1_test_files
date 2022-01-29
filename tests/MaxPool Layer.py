089c8e38126900c9069c9e8971ad0367bf3ad2af
lavinama <70475094+lavinama@users.noreply.github.com>
2022-01-29 10:38:36 +0000

    Delete tests directory

--- tests/MaxPool Layer.py
+++
@@ -1,8 +0,0 @@
-test = {   'name': 'MaxPool Layer',
-    'points': 15,
-    'suites': [   {   'cases': [   {'code': '>>> list(MaxPool2d(3)(torch.zeros((10,3,64,64))).shape) == [10,3,21,21]\nTrue', 'hidden': False, 'locked': False, 'points': 0},
-                                   {'code': '>>> type(MaxPool2d(3)(torch.zeros((10,3,64,64)))) in [torch.Tensor]\nTrue', 'hidden': False, 'locked': False, 'points': 0}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
